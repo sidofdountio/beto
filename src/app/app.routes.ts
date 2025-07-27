2fe9035c833b0f242e1dd3f44eb293e4e2317402
sidof <sidofdountio406@gmail.com>
2025-07-27 10:51:20 +0100

    beto web site in progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './web-site/about/about.component';
+import { PricingComponent } from './web-site/pricing/pricing.component';
+import { WelcomeComponent } from './web-site/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: WelcomeComponent
+    },
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "home"
+    },
+    {
+        path: "pricing",
+        component: PricingComponent,
+        title: 'Pricing'
+    },
+    {
+        path: "about",
+        component: AboutComponent,
+        title: 'about'
+    },
+
+    
+   
+    {
+        path: "**",
+        title: "page not found",
+        component: PageNotFoundComponent
+    }
+];
